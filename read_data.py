e06ba26207b5237d3aea730d22824586a1530936
Zoe Goss <zg811@imperial.ac.uk>
2018-03-19 15:08:31 +0000

    Making the merg Tom's problem

--- read_data.py
+++ read_data.py
@@ -2,48 +2,50 @@ import pandas as pd
 import math
 import numpy as np
 
-# # Load csv ad a datafarme object using pandas
+
 # Region_1_DR=pd.read_csv('../AIR_data/Correct_loss_data/Region_1_DR.csv')
 # print(list(Region_1_DR))
 
-# # Select unique location ID in the AIRSID column
+
 # unique_arsid=Region_1_DR.AIRSID.unique()
 # for air_id in unique_arsid :
 # 	# For each unique AIRSID finr the mean of LOB1
 # 	print(air_id, Region_1_DR.loc[Region_1_DR['AIRSID']==air_id, 'LOB4' ].sum())
 		
 def getLOBdata(csvname):
+	# Load csv ad a dataframe object using pandas
 	region_data=pd.read_csv(csvname)
+	# Get all of the headings from the csv
 	headernames = list(region_data)
+	# Get the names of all of the LOB columns, eg LOB1, LOB2 etc
 	lobnames = headernames[3:3+6]
+	# Select unique location ID in the AIRSID colum
 	unique_arsid=region_data['AIRSID'].unique() # change to region_data.headernames[0] to make more flexible
+	# Initialise empty nan 2d array, 1 column airsid, following columns are the LOBS
 	data_output = np.zeros([len(unique_arsid), len(lobnames)+1])
 	data_output.fill(np.nan)
-	print(data_output.shape)
+	delete_list = [] # prep list for empty columns to delete
 
 	for j, lob in enumerate(lobnames):
 		for i, air_id in enumerate(unique_arsid):
+			# Populate data_ouput with the AIRSID and the sum of each LOB
 			data_output[i, 0] = air_id 
 			lobsum = region_data.loc[region_data['AIRSID']==air_id, lob  ].sum()
-			if math.isnan(lobsum)=='True':
-				print('No data for ',lobsum, ', skipping this data')
-			else:
-				data_output[i, j+1] = lobsum
+			data_output[i, j+1] = lobsum
+		# If a column has no LOB data, save the index to delete it
+		if math.isnan(data_output[0, j+1]):
+			delete_list.append(j+1)			
 
+	# If a column has no LOB data, delete it
+	data_output = np.delete(data_output, delete_list, axis=1)
+	# Delete the names of columns not included for the dataframe
+	column_names = ['AIRSID']+lobnames
+	
+	column_names = np.delete(column_names, delete_list, axis=1)
+	df_output = pd.DataFrame(data_output, columns=column_names)
 
-	# i=1
-	# for lob in lobnames:
-	# 	print(lob)
-	# 	j=0
-	# 	for air_id in unique_arsid:
-	# 		data_output[j, 0] = air_id 
-	# 		lobsum = region_data.loc[region_data['AIRSID']==air_id, lob  ].sum()
-	# 		if math.isnan(lobsum)=='True':
-	# 			print('No data for ',lobsum, ', skipping this data')
-	# 		else:
-	# 			data_output[j, i] = lobsum
-	# 		j+=1
-	# 	i +=1
-	print(data_output)
+	return df_output
 
-getLOBdata('../AIR_data/Correct_loss_data/Region_1_DR.csv')
+region1data = getLOBdata('../AIR_data/Correct_loss_data/Region_1_DR.csv')
+
+print(region1data)
